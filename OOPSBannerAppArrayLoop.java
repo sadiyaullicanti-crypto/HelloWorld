920d97913abbd874c76ca62031117a23a1055194
sadiyaullicanti-crypto <sadiyaullicanti@gmail.com>
2026-02-23 19:51:04 +0530

    UC4 - Implemented OOPS banner using Array and Loop

---
+++ OOPSBannerAppArrayLoop.java
@@ -0,0 +1,17 @@
+public class OOPSBannerAppArrayLoop {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+                "***********************",
+                "*                     *",
+                "*      HELLO OOPS     *",
+                "*                     *",
+                "***********************"
+        };
+
+        for (int i = 0; i < banner.length; i++) {
+            System.out.println(banner[i]);
+        }
+    }
+}
